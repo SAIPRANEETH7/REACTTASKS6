f605ee7126c3b04593becc8ed57bfe94df297737
SAIPRANEETH7 <avadhanampraneeth@gmail.com>
2023-10-15 21:39:14 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+// Contact.js
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>I'm always open to new opportunities and collaborations.</p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href=" ">avadhanampraneeth@gmail.com</a>
+        </li>
+        <li>
+          <strong>Instagram:</strong>{" "}
+          <a href=" " target="_blank" rel="noopener noreferrer">
+            @donpraneeth_
+          </a>
+        </li>
+        <li>
+          <strong>GitHub:</strong>{" "}
+          <a href=" " target="_blank" rel="noopener noreferrer">
+            https://github.com/SAIPRANEETH7/REACTTASKS
+          </a>
+        </li>
+      </ul>
+      <p>I'm looking forward to connecting with you!</p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      {"BBC_news,VIT_news,GEARAGETECH,REPAIRPOINT,MUSICON"}
+    </section>
+  );
+};
+
+export default Projects;
